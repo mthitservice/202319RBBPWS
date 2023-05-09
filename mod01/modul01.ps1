dc7a60442ff6122a48542cc17cbb417d454d1584
Michael Lindner <Michael.Lindner@mth-it-service.com>
2023-05-09 09:02:28 +0200

    Start

---
+++ mod01/modul01.ps1
@@ -0,0 +1,33 @@
+﻿#Command Beispiel
+Get-Command *net*
+
+#Get-Help Beispiel
+Get-Help *net* -Category Cmdlet
+
+# About
+get-help about*
+
+get-help about_aliases
+
+get-help about_eventlogs -ShowWindow
+
+#Test
+cls
+get-help *beep*
+
+#Aliases
+
+Get-ChildItem
+
+Get-Alias dir
+
+New-Alias list Get-ChildItem
+
+Get-Alias -Definition Get-Childitem
+
+#Module
+Get-Module
+
+Get-Module -ListAvailable
+
+Import-Module WindowsUpdate
