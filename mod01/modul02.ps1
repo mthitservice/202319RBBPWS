162320eb6cdd24be4da8da17d901c012ca230b60
Michael Lindner <Michael.Lindner@mth-it-service.com>
2023-05-09 09:02:43 +0200

    Start

---
+++ mod01/modul02.ps1
@@ -0,0 +1,14 @@
+﻿# Start Skriptaufzeichnung
+Start-Transcript C:\Skripte\transkript\log.txt
+
+# Netzbefehle
+Get-NetIPConfiguration
+
+Get-NetIPAddress
+
+Get-DnsClientServerAddress
+
+Get-NetRoute
+
+
+Get-AdObject -Filter "ObjectClass -eq 'contact'"
